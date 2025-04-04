0f4376be16aac5c06133761e085758b809f81e2c
kileldylan <kileldylan@gmail.com>
2025-04-04 12:37:54 +0300

    updated the settings of security key

--- edulog_backend/edulog/settings.py
+++ edulog_backend/edulog/settings.py
@@ -25,7 +25,7 @@ load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '&)cl0^$jtry@lydp%bb19*6y@9a)t^_yblq1@859ety_zglc$f'
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split()
